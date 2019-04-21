5e41ccca04577eea39874b144b6b399423650e95
AqsaZahid256 <aqsazahid938@gmail.com>
2019-04-22 01:52:11 +0500

    layout setting and home page

---
+++ DB43/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DB43.Startup))]
+namespace DB43
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
